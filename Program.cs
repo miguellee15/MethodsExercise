9d287648da223b3c27f95f9535d46749dcfa1311
miguellee15 <miguellee15@gmail.com>
2020-10-23 11:43:56 -0400

    done

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------Exercise 1--------
+            //Name: Michael
+            //Favorite Color: Blue
+            //Favorite Animal: Walrus
+            //Favorite Band: The Beatles
+
+
+            Console.Write("Hello, what is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.Write($"Hello, {userName}! What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.Write($"{color} is an awsome color, what is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.Write($"I absolutley love that animal, what is your favorite band?");
+            var band = Console.ReadLine();
+
+
+        }
+    }
+}
